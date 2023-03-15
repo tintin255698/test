c15d6ff33fd35fd8e7ea01fadad39565f14ad49d
v.joly <v.joly@caplaser.fr>
2023-03-15 16:03:16 +0100

    test commit 1 version desktop

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let card = document.querySelectorAll(".card");
+let contentBox = document.querySelectorAll(".contentBox");
+
+for (let i = 0; i < card.length; i++) {
+    card[i].addEventListener("mouseover", function () {
+        for (let i = 0; i < contentBox.length; i++) {
+            contentBox[i].className = "contentBox";
+        }
+        document.getElementById(this.dataset.id).className = "contentBox active";
+
+        for (let i = 0; i < card.length; i++) {
+            card[i].className = "card";
+        }
+        this.className = "card active";
+    });
+}
